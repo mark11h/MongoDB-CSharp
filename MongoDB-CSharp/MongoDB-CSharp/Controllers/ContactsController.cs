ed3c89b36f136a7505d9b554bb07482b4294d18c
Mark Hinchliffe <mark11h@live.co.uk>
2015-07-14 20:57:13 +0100

    Added ability to edit existing contact

--- MongoDB-CSharp/MongoDB-CSharp/Controllers/ContactsController.cs
+++ MongoDB-CSharp/MongoDB-CSharp/Controllers/ContactsController.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -32,6 +33,7 @@ namespace MongoDB_CSharp.Controllers
             {
                 contacts.Add(new Models.Contact
                 {
+                    Id = item["_id"].AsObjectId,
                     FirstName = item["firstname"].AsString,
                     Surname = item["surname"].AsString
                 });
@@ -60,6 +62,52 @@ namespace MongoDB_CSharp.Controllers
                 
                 contactsCollection.InsertOneAsync(contact);
 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Edit(string id)
+        {
+            var contactsCollection = RetreiveMongohqDb().GetCollection<BsonDocument>(CollectionName);
+
+            FilterDefinition<BsonDocument> filter = new BsonDocument("_id", ObjectId.Parse(id));
+
+            var contactToEdit = await contactsCollection.Find(filter).FirstOrDefaultAsync();
+
+            if (contactToEdit == null) return View("Index");
+
+            var contact = new Models.Contact
+            {
+                Id = contactToEdit["_id"].AsObjectId,
+                FirstName = contactToEdit["firstname"].AsString,
+                Surname = contactToEdit["surname"].AsString
+            };
+            
+            return View(contact);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FormCollection collection)
+        {
+            try
+            {
+                var updatedContact = new BsonDocument
+                {
+                    { "_id", ObjectId.Parse(collection["id"]) },
+                    { "firstname", collection["FirstName"] },
+                    { "surname", collection["Surname"] }
+                };
+
+                FilterDefinition<BsonDocument> filter = new BsonDocument("_id", ObjectId.Parse(collection["id"]));
+
+                var contactsCollection = RetreiveMongohqDb().GetCollection<BsonDocument>(CollectionName);
+
+                contactsCollection.ReplaceOneAsync(filter, updatedContact);
+                
                 return RedirectToAction("Index");
             }
             catch
--- MongoDB-CSharp/MongoDB-CSharp/Models/Contact.cs
+++ MongoDB-CSharp/MongoDB-CSharp/Models/Contact.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel;
+using MongoDB.Bson;
 
 namespace MongoDB_CSharp.Models
 {
     public class Contact
     {
+        public ObjectId Id { get; set; }
+
         [DisplayName("First Name")]
         public string FirstName { get; set; }
 
